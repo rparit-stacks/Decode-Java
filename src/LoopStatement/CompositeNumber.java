6a7b35ec07fa5f23fa72a8b54d901c0bb51d4786
Rohit Parit <rohitparit1934@gmail.com>
2024-08-18 13:01:50 +0530

    arrays

--- src/LoopStatement/CompositeNumber.java
+++ src/LoopStatement/CompositeNumber.java
@@ -6,16 +6,21 @@ public class CompositeNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int counter = 0;
-        //Composite Number
+        // Composite Number
         System.out.print("Please Enter a Number : ");
         int number = sc.nextInt();
 
+        sc.close();
+
         for (int i = 1; i < number; i++) {
-            if (number % i == 0) counter++;
+            if (number % i == 0)
+                counter++;
         }
 
-        if (counter >= 2) System.out.println(number + " Is a Composite Number");
-        else System.out.println(number + " Is NOT a Composite Number");
+        if (counter >= 2)
+            System.out.println(number + " Is a Composite Number");
+        else
+            System.out.println(number + " Is NOT a Composite Number");
     }
 
 }
